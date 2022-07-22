4fce367cc6b3687183a791f05a6f7dc38da6b8df
Venkata199798 <96472180+Venkata199798@users.noreply.github.com>
2022-07-22 08:44:55 +0530

    Add files via upload

---
+++ temp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "./index.html")
+
+def home(request):
+    return render(request, "./firstone.html")
+
+
+    '''
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+'''
